63b0a9a3e803ec0cd320e3c7c3b99659f80163bb
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-02-07 19:54:10 -0300

    add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swaggerUI from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerFile from "./swagger.json";
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
